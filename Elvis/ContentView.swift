f5010a62f28e4657dbc06727f185d50f8818f61b
Raemur <raemur@gmail.com>
2025-06-08 20:47:42 -0700

    SwiftUI Challenge: The Elvis Costello Peace, Love, and Understanding App (2025)

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var image = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .foregroundStyle(.purple)
+                .fontWeight(.black)
+                .shadow(radius: 3)
+            
+            Spacer()
+            Image(systemName: image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            Text(message)
+                .foregroundStyle(.purple)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .shadow(radius: 3)
+            Spacer()
+            HStack {
+                Button("Peace") {
+                    message = "Peace"
+                    image =  "peacesign"
+                }
+            
+                Button("Love") {
+                    message = "Love"
+                    image = "heart"
+                }
+                Button("Understanding") {
+                    message = "Understanding"
+                    image = "lightbulb"
+                }
+                    
+            }.foregroundStyle(.white)
+                .buttonStyle(.borderedProminent)
+                    .tint(.purple)
+                
         }
         .padding()
     }
