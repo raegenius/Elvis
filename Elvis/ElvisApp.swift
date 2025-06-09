9ca4af5cfcaedd9e0e765ef2f81096f62ad90543
Raemur <raemur@gmail.com>
2025-06-08 19:31:54 -0700

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Raemur Bedward on 2025-06-08.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
